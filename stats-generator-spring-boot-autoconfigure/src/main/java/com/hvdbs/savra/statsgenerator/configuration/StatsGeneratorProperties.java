ae18c079b13aff65217dbefa21d9d6531e7388e2
Sergey Savrukhin <savra.sv@yandex.ru>
2024-05-07 21:51:38 +0300

    add setter to props

--- stats-generator-spring-boot-autoconfigure/src/main/java/com/hvdbs/savra/statsgenerator/configuration/StatsGeneratorProperties.java
+++ stats-generator-spring-boot-autoconfigure/src/main/java/com/hvdbs/savra/statsgenerator/configuration/StatsGeneratorProperties.java
@@ -1,10 +1,12 @@
 package com.hvdbs.savra.statsgenerator.configuration;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 
+@Setter
 @Getter
 @ConfigurationProperties(prefix = "stats-generator")
 public class StatsGeneratorProperties {
